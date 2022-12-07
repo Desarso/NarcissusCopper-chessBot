69b9fcfbdd0f83acb15d02e9d5eb6849493adb24
Desarso <malek.gabriel33@gmail.com>
2022-12-06 17:10:13 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import type { Component } from 'solid-js';
+
+import '../css/styles.css'
+
+const App: Component = () => {
+  return (
+    <div>
+      <h1>Hello, Solid!</h1>
+    </div>
+  );
+};
+
+export default App;
