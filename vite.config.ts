72168e74008b6b0c51bc2a09e25cd1380853d53e
Desarso <malek.gabriel33@gmail.com>
2022-12-06 17:16:34 -0800

    all

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import path from "path";
 
 export default defineConfig({
   plugins: [solidPlugin()],
+  base: "",
   server: {
     port: 3000,
   },
   build: {
     target: 'esnext',
+    outDir: 'docs',
   },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "/src"),
+      "~@": path.resolve(__dirname, "/src"),
+    }
+  }
 });
